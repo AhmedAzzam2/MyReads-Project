7a6b7442e8d202090624230b2391fcd287b98d4f
AhmedAzzam2 <ahmedaz201811@gmail.com>
2022-05-01 21:44:58 +0200

    const [params,setparams] = useState(useParams());

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+// import browser router and routes and route components v6 from react-router-dom
+import {  BrowserRouter, Routes, Route, } from "react-router-dom";
+
 import "./index.css";
 import App from "./App";
+import Single from "./single";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  
+
+<BrowserRouter>
+<Routes>
+  <Route path="/" element={<App />} />
+    <Route path="single/:id" element={<Single />} />
+</Routes>
+</BrowserRouter>,
   document.getElementById("root")
 );
